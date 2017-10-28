c0752acc6698f1d3df239eb6816cce6253065ea8
Christopher Pitt <cgpitt@gmail.com>
2017-10-28 23:36:30 +0300

    Refactor for new plugin

--- source/parsers.php
+++ source/parsers.php
@@ -3,8 +3,15 @@
 namespace Yay;
 
 const LAYER_DELIMITERS_WITH_TRAILING_COMMA = [
+    '{' => 1,
+    T_CURLY_OPEN => 1,
+    T_DOLLAR_OPEN_CURLY_BRACES => 1,
+    '}' => -1,
+    '[' => 2,
+    ']' => -2,
+    '(' => 3,
     ',' => -3,
-] + LAYER_DELIMITERS;
+];
 
 function layer_with_trailing_comma(array $delimiters = LAYER_DELIMITERS_WITH_TRAILING_COMMA) : Parser
 {
